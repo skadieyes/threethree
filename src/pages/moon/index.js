9de910570e3c60382821985dfc986ce4bda51e83
郑染秋 <cat.mul@zhengranqiu.local>
2018-09-21 19:09:01 +0800

    update

--- src/pages/moon/index.js
+++ src/pages/moon/index.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+import THREE from 'three'
 
-class Moon extends Component {
-  render() {
-    return (
-      <div className="App">
-        111
-      </div>
-    );
-  }
+let camera,
+    scene,
+    renderer,
+    cube;
+
+class Moon extends React.Component {
+    constructor() {
+        super();
+        this.scence();
+    }
+    render() {
+        return <div onClick={this.riseUp.bind(this)}>11111111111</div>;
+    }
+    scence() {
+        camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.01, 10);
+        camera.position.z = 5;
+
+        scene = new THREE.Scene();
+
+        let geometry = new THREE.BoxGeometry(1, 1, 1);
+        let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        let hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, .9);
+        let ambientLight = new THREE.AmbientLight(0xdc8874, .5);
+        scene.add(hemisphereLight);
+        scene.add(ambientLight);
+        renderer.render(scene, camera);
+        document.body.appendChild(renderer.domElement);
+        renderer.render(scene, camera);
+    }
+    riseUp() {
+        cube.rotation.x += 1;
+        cube.position.y += 0.1;
+        cube.rotation.y += 1;
+        renderer.render(scene, camera);
+        requestAnimationFrame(this.riseUp.bind(this));
+    }
 }
 
 export default Moon;
